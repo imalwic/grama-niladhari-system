e6221ccfb15ea8c2d45fea3d689172cf8f45da41
Imal <imalwic2023@gmail.com>
2026-09-07 11:45:00 +0530

    Inject OfflineIndicator to root layout

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
+import { OfflineIndicator } from '@/components/OfflineIndicator';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,6 +47,7 @@ export default async function RootLayout({
       <body className="min-h-full flex flex-col">
         <NextIntlClientProvider messages={messages}>
           {children}
+          <OfflineIndicator />
         </NextIntlClientProvider>
       </body>
     </html>
