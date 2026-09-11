b458723b010f58cec5ff7a8b3a44b99b92955408
Imal <imalwic2023@gmail.com>
2026-09-11 22:27:39 +0530

    fix: update sidebar links to use i18n routing to resolve 404 errors

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
+import { Link, usePathname } from "@/i18n/routing";
 import { useTranslations } from "next-intl";
 import { 
   LayoutDashboard, 
