daab0ed8a864fe375fc4f8e2db963fa5d6267ea8
Imal <imalwic2023@gmail.com>
2026-09-13 08:25:48 +0530

    feat: add complete list of provinces, districts, and local authorities to sl-data

--- frontend/src/lib/sl-data.ts
+++ frontend/src/lib/sl-data.ts
@@ -7,38 +7,17 @@ export const SRI_LANKA_DATA = {
         {
           id: "colombo",
           name: "Colombo",
-          localAuthorities: ["Colombo MC", "Dehiwala-Mount Lavinia MC", "Moratuwa MC", "Kotte MC", "Homagama PS", "Maharagama UC"]
+          localAuthorities: ["Colombo MC", "Dehiwala-Mount Lavinia MC", "Moratuwa MC", "Sri Jayawardenepura Kotte MC", "Kolonnawa UC", "Seethawakapura UC", "Maharagama UC", "Boralesgamuwa UC", "Kesbewa UC", "Kotikawatta Mulleriyawa PS", "Kaduwela MC", "Homagama PS", "Seethawaka PS", "Padukka PS"]
         },
         {
           id: "gampaha",
           name: "Gampaha",
-          localAuthorities: ["Gampaha MC", "Negombo MC", "Kelaniya PS", "Mahara PS", "Biyagama PS"]
+          localAuthorities: ["Gampaha MC", "Negombo MC", "Minuwangoda UC", "Peliyagoda UC", "Wattala Mabola UC", "Ja-Ela UC", "Katunayake Seeduwa UC", "Biyagama PS", "Divulapitiya PS", "Dompe PS", "Gampaha PS", "Ja-Ela PS", "Katana PS", "Kelaniya PS", "Mahara PS", "Meerigama PS", "Minuwangoda PS", "Wattala PS", "Attanagalla PS"]
         },
         {
           id: "kalutara",
           name: "Kalutara",
-          localAuthorities: ["Kalutara UC", "Panadura UC", "Horana UC", "Matugama PS"]
-        }
-      ]
-    },
-    {
-      id: "southern",
-      name: "Southern Province",
-      districts: [
-        {
-          id: "hambantota",
-          name: "Hambantota",
-          localAuthorities: ["Hambantota MC", "Tangalle UC", "Weeraketiya PS", "Beliatta PS", "Ambalantota PS"]
-        },
-        {
-          id: "galle",
-          name: "Galle",
-          localAuthorities: ["Galle MC", "Ambalangoda UC", "Hikkaduwa UC", "Baddegama PS", "Elpitiya PS"]
-        },
-        {
-          id: "matara",
-          name: "Matara",
-          localAuthorities: ["Matara MC", "Weligama UC", "Dickwella PS", "Hakmana PS"]
+          localAuthorities: ["Kalutara UC", "Beruwala UC", "Panadura UC", "Horana UC", "Agalawatta PS", "Bandaragama PS", "Beruwala PS", "Bulathsinhala PS", "Dodangoda PS", "Horana PS", "Kalutara PS", "Madurawela PS", "Mathugama PS", "Millaniya PS", "Palindanuwara PS", "Panadura PS", "Walallawita PS"]
         }
       ]
     },
@@ -49,20 +28,156 @@ export const SRI_LANKA_DATA = {
         {
           id: "kandy",
           name: "Kandy",
-          localAuthorities: ["Kandy MC", "Gampola UC", "Nawalapitiya UC", "Kundasale PS"]
+          localAuthorities: ["Kandy MC", "Gampola UC", "Nawalapitiya UC", "Wattegama UC", "Kadugannawa UC", "Akurana PS", "Delthota PS", "Doluwa PS", "Ganga Ihala Korale PS", "Harispattuwa PS", "Kundanysale PS", "Medadumbara PS", "Minipe PS", "Panvila PS", "Pasbage Korale PS", "Pathadumbara PS", "Pathahewaheta PS", "Poojapitiya PS", "Thumpane PS", "Udadumbara PS", "Udapalatha PS", "Udunuwara PS", "Yatinuwara PS", "Ganga Wata Korale PS"]
         },
         {
           id: "matale",
           name: "Matale",
-          localAuthorities: ["Matale MC", "Dambulla MC", "Rattota PS"]
+          localAuthorities: ["Matale MC", "Dambulla MC", "Ambanganga Korale PS", "Dambulla PS", "Galewela PS", "Laggala Pallegama PS", "Matale PS", "Naula PS", "Pallepola PS", "Rattota PS", "Ukuwela PS", "Wilgamuwa PS", "Yatawatta PS"]
         },
         {
           id: "nuwara-eliya",
           name: "Nuwara Eliya",
-          localAuthorities: ["Nuwara Eliya MC", "Hatton-Dickoya UC", "Talawakele-Lindula UC", "Kotagala PS"]
+          localAuthorities: ["Nuwara Eliya MC", "Hatton-Dickoya UC", "Talawakele-Lindula UC", "Ambagamuwa PS", "Hanguranketha PS", "Kothmale PS", "Nuwara Eliya PS", "Walapane PS", "Agarapathana PS", "Kotagala PS", "Maskeliya PS", "Norwood PS"]
+        }
+      ]
+    },
+    {
+      id: "southern",
+      name: "Southern Province",
+      districts: [
+        {
+          id: "galle",
+          name: "Galle",
+          localAuthorities: ["Galle MC", "Ambalangoda UC", "Hikkaduwa UC", "Akmeemana PS", "Ambalangoda PS", "Baddegama PS", "Balapitiya PS", "Benthota PS", "Bope Poddala PS", "Elpitiya PS", "Galle PS", "Habaraduwa PS", "Hikkaduwa PS", "Imaduwa PS", "Karandeniya PS", "Nagoda PS", "Neluwa PS", "Niyagama PS", "Thawalama PS", "Welivitiya Divithura PS", "Yakkalamulla PS"]
+        },
+        {
+          id: "matara",
+          name: "Matara",
+          localAuthorities: ["Matara MC", "Weligama UC", "Akuressa PS", "Athuraliya PS", "Devinuwara PS", "Dickwella PS", "Hakmana PS", "Kamburupitiya PS", "Kirinda Puhulwella PS", "Kotapola PS", "Malimbada PS", "Matara PS", "Mulatiyana PS", "Pasgoda PS", "Pitabeddara PS", "Thihagoda PS", "Weligama PS"]
+        },
+        {
+          id: "hambantota",
+          name: "Hambantota",
+          localAuthorities: ["Hambantota MC", "Tangalle UC", "Ambalantota PS", "Angunakolapelessa PS", "Beliatta PS", "Hambantota PS", "Katuwana PS", "Lunugamwehera PS", "Sooriyawewa PS", "Tangalle PS", "Tissamaharama PS", "Walasmulla PS", "Weeraketiya PS"]
+        }
+      ]
+    },
+    {
+      id: "northern",
+      name: "Northern Province",
+      districts: [
+        {
+          id: "jaffna",
+          name: "Jaffna",
+          localAuthorities: ["Jaffna MC", "Valvettithurai UC", "Chavakachcheri UC", "Point Pedro UC", "Delft PS", "Island North (Kayts) PS", "Island South (Velanai) PS", "Jaffna PS", "Nallur PS", "Karainagar PS", "Vadamaradchi Southwest (Karaveddy) PS", "Vadamaradchi North (Point Pedro) PS", "Vadamaradchi East (Maruthankerney) PS", "Valikamam East (Kopay) PS", "Valikamam North (Tellippalai) PS", "Valikamam South (Uduvil) PS", "Valikamam Southwest (Sandilipay) PS", "Valikamam West (Chankanai) PS", "Thenmaradchi (Chavakachcheri) PS"]
+        },
+        {
+          id: "kilinochchi",
+          name: "Kilinochchi",
+          localAuthorities: ["Karachchi PS", "Pachchilaipalli PS", "Poonakary PS"]
+        },
+        {
+          id: "mannar",
+          name: "Mannar",
+          localAuthorities: ["Mannar UC", "Mannar PS", "Manthai West PS", "Musali PS", "Nanaddan PS"]
+        },
+        {
+          id: "vavuniya",
+          name: "Vavuniya",
+          localAuthorities: ["Vavuniya UC", "Vavuniya North PS", "Vavuniya South PS", "Vengalacheddikulam PS"]
+        },
+        {
+          id: "mullaitivu",
+          name: "Mullaitivu",
+          localAuthorities: ["Manthai East PS", "Maritimepattu PS", "Puthukkudiyiruppu PS", "Thunukkai PS"]
+        }
+      ]
+    },
+    {
+      id: "eastern",
+      name: "Eastern Province",
+      districts: [
+        {
+          id: "batticaloa",
+          name: "Batticaloa",
+          localAuthorities: ["Batticaloa MC", "Eravur UC", "Kattankudy UC", "Eravur Pattu PS", "Koralaipattu (Valaichchenai) PS", "Koralaipattu North (Vaharai) PS", "Koralaipattu West (Oddamavadi) PS", "Manmunai South & Eruvil Pattu PS", "Manmunai Southwest (Paddippalai) PS", "Manmunai West (Vavamunathivu) PS", "Porativu Pattu PS", "Koralai Pattu Central PS"]
+        },
+        {
+          id: "ampara",
+          name: "Ampara",
+          localAuthorities: ["Kalmunai MC", "Ampara UC", "Addalachchenai PS", "Akkaraipattu PS", "Alayadivembu PS", "Ampara PS", "Damana PS", "Dehiattakandiya PS", "Irakkamam PS", "Karaitivu PS", "Lahugala PS", "Maha Oya PS", "Navithanveli PS", "Nintavur PS", "Padiyathalawa PS", "Pottuvil PS", "Samanthurai PS", "Thirukkovil PS", "Uhana PS", "Sammanthurai PS"]
+        },
+        {
+          id: "trincomalee",
+          name: "Trincomalee",
+          localAuthorities: ["Trincomalee UC", "Kinniya UC", "Gomarankadawala PS", "Kantalai PS", "Kinniya PS", "Kuchchaveli PS", "Morawewa PS", "Muttur PS", "Padavi Sri Pura PS", "Seruvila PS", "Thampalakamam PS", "Town and Gravets PS", "Verugal PS"]
+        }
+      ]
+    },
+    {
+      id: "north-western",
+      name: "North Western Province",
+      districts: [
+        {
+          id: "kurunegala",
+          name: "Kurunegala",
+          localAuthorities: ["Kurunegala MC", "Kuliyapitiya UC", "Alawwa PS", "Bingiriya PS", "Galgamuwa PS", "Giribawa PS", "Ibbagamuwa PS", "Kobeigane PS", "Kotawehera PS", "Kuliyapitiya PS", "Kurunegala PS", "Maho PS", "Mallawapitiya PS", "Maspotha PS", "Mawathagama PS", "Narammala PS", "Nikaweratiya PS", "Panduwasnuwara PS", "Pannala PS", "Polgahawela PS", "Polpithigama PS", "Ridigama PS", "Udubaddawa PS", "Wariyapola PS"]
+        },
+        {
+          id: "puttalam",
+          name: "Puttalam",
+          localAuthorities: ["Puttalam UC", "Chilaw UC", "Anamaduwa PS", "Arachchikattuwa PS", "Chilaw PS", "Dankotuwa PS", "Kalpitiya PS", "Karuwalagaswewa PS", "Madampe PS", "Mahawewa PS", "Mundalama PS", "Nattandiya PS", "Nawagattegama PS", "Pallama PS", "Puttalam PS", "Vanathavilluwa PS", "Wennappuwa PS"]
+        }
+      ]
+    },
+    {
+      id: "north-central",
+      name: "North Central Province",
+      districts: [
+        {
+          id: "anuradhapura",
+          name: "Anuradhapura",
+          localAuthorities: ["Anuradhapura MC", "Galnewa PS", "Galenbindunuwewa PS", "Horowpothana PS", "Ipalogama PS", "Kahatagasdigiliya PS", "Kebithigollewa PS", "Kekirawa PS", "Medawachchiya PS", "Mihinthale PS", "Nochchiyagama PS", "Nuwaragam Palatha Central PS", "Nuwaragam Palatha East PS", "Padaviya PS", "Palagala PS", "Palugaswewa PS", "Rajanganaya PS", "Rambewa PS", "Thalawa PS", "Thambuttegama PS", "Thirappane PS"]
+        },
+        {
+          id: "polonnaruwa",
+          name: "Polonnaruwa",
+          localAuthorities: ["Polonnaruwa MC", "Dimbulagala PS", "Elahera PS", "Hingurakgoda PS", "Lankapura PS", "Medirigiriya PS", "Thamankaduwa PS", "Welikanda PS"]
+        }
+      ]
+    },
+    {
+      id: "uva",
+      name: "Uva Province",
+      districts: [
+        {
+          id: "badulla",
+          name: "Badulla",
+          localAuthorities: ["Badulla MC", "Bandarawela MC", "Haputale UC", "Badulla PS", "Bandarawela PS", "Ella PS", "Haldummulla PS", "Hali-Ela PS", "Haputale PS", "Kandaketiya PS", "Lunugala PS", "Mahiyanganaya PS", "Meegahakivula PS", "Passara PS", "Rideemaliyadda PS", "Soranathota PS", "Uva Paranagama PS", "Welimada PS"]
+        },
+        {
+          id: "moneragala",
+          name: "Moneragala",
+          localAuthorities: ["Badalkumbura PS", "Bibile PS", "Buttala PS", "Kataragama PS", "Madulla PS", "Medagama PS", "Moneragala PS", "Siyambalanduwa PS", "Thanamalwila PS", "Wellawaya PS"]
+        }
+      ]
+    },
+    {
+      id: "sabaragamuwa",
+      name: "Sabaragamuwa Province",
+      districts: [
+        {
+          id: "ratnapura",
+          name: "Ratnapura",
+          localAuthorities: ["Ratnapura MC", "Balangoda UC", "Ayagama PS", "Balangoda PS", "Eheliyagoda PS", "Elapattha PS", "Embilipitiya PS", "Godakawela PS", "Imbulpe PS", "Kahawatta PS", "Kalawana PS", "Kuruwita PS", "Nivithigala PS", "Opanayaka PS", "Pelmadulla PS", "Ratnapura PS", "Weligepola PS", "Embilipitiya UC"]
+        },
+        {
+          id: "kegalle",
+          name: "Kegalle",
+          localAuthorities: ["Kegalle UC", "Aranayaka PS", "Bulathkohupitiya PS", "Dehiovita PS", "Deraniyagala PS", "Galigamuwa PS", "Kegalle PS", "Mawanella PS", "Rambukkana PS", "Ruwanwella PS", "Warakapola PS", "Yatiyantota PS"]
         }
       ]
     }
-    // Note: Other provinces can be added similarly
   ]
 };
